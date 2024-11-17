179bc1e7e8813c0e7fea672ee68d1d86b77cafca
Soyeon <limcatcat@gmail.com>
2024-11-17 20:55:18 +0100

    .gitignore file added

--- daily_kanban/daily_kanban/settings.py
+++ daily_kanban/daily_kanban/settings.py
@@ -11,10 +11,14 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# STATICFILES_DIR = [
+#     os.path.join(BASE_DIR, '/frontend/static')
+# ]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -118,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- daily_kanban/frontend/src/components/App.js
+++ daily_kanban/frontend/src/components/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from "react";
-import render from "react-dom";
 
 export default class App extends Component {
     constructor(props) {
         super(props);
     }
 
-    render() {
-        return <h1>Quokka Princess</h1>;
+    render(){
+        return(<h1>Quokka Princess</h1>)
     }
-
 }
 
 const appDiv = document.getElementById("app");
