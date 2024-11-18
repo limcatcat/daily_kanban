522b194af455956792b91cde7a54f96304930e0c
Soyeon <limcatcat@gmail.com>
2024-11-18 09:33:32 +0100

    Task component created

--- daily_kanban/frontend/src/components/Task.js
+++ daily_kanban/frontend/src/components/Task.js
@@ -1,3 +1,8 @@
-function Task(props) {
-    
+import React from 'react';
+import '../../static/css/task.css'
+
+function Task({description}) {
+    return <div class='task'>{description}</div>;
 }
+
+export default Task;
