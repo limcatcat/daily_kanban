1ffbe3efcdb00795325ebf55189b697a15b32bb2
Soyeon <limcatcat@gmail.com>
2024-11-19 15:57:05 +0100

    different colors for Task components depending on task status

--- daily_kanban/frontend/src/components/KanbanColumn.js
+++ daily_kanban/frontend/src/components/KanbanColumn.js
@@ -3,13 +3,14 @@ import Task from './Task';
 
 
 function KanbanColumn({title, tasks}) {
+
     return (
         <div className='column'>
             <h3>{title}</h3>
             <ul>
                 {tasks.map(task => (
                     <li key={task.id}>
-                        <Task description={task.description} />
+                        <Task description={task.description} status={task.status} />
                     </li>
                 ))}
             </ul>
--- daily_kanban/frontend/src/components/Task.js
+++ daily_kanban/frontend/src/components/Task.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import '../../static/css/task.css'
 
-function Task({description}) {
+function Task(props) {
+
+    let taskStatus = '';
+    
+    if (props.status == 'Backlog')
+        taskStatus = 'backlog'
+    else if (props.status == 'Today')
+        taskStatus = 'today'
+    else if (props.status == 'In Progress')
+        taskStatus = 'in-progress'
+    else if (props.status == 'Done')
+        taskStatus = 'done'
+
+
     return (
-        <div className='task'>
-            <p>{description}</p>
+        <div className={`task ${taskStatus}`}>
+            <p>{props.description}</p>
         </div>
     );
 }
