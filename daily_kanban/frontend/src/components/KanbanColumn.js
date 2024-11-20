d30fb201066f5c5578e4ecc7fa97b1fe7893e860
Soyeon <limcatcat@gmail.com>
2024-11-20 03:42:56 +0100

    fixed minor bugs when moving a Task component across columns (Task disappearing & made Column components droppable area)

--- daily_kanban/frontend/src/components/KanbanColumn.js
+++ daily_kanban/frontend/src/components/KanbanColumn.js
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
 import Task from './Task';
 import { SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable';
+import { useDroppable } from '@dnd-kit/core';
 
 
-function KanbanColumn({title, tasks, status, setTasks}) {
+function KanbanColumn({title, tasks, status}) {
 
     const tasksByStatus = tasks.filter(task => task.status === status);
 
+    const {setNodeRef, isOver} = useDroppable({id:status});
+
     return (
-        <div className='column'>
+        <div ref={setNodeRef} className={`column ${isOver ? 'highlight' : ''}`}>
             <SortableContext items={tasksByStatus.map(task => task.id)} strategy={verticalListSortingStrategy}>
             <h3>{title}</h3>
            
