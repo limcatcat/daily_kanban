aac06d0a0470742e2ae1fe14cfb87e6d74d64d1f
Soyeon <limcatcat@gmail.com>
2024-12-28 19:11:50 +0100

    date_assigned/date_done filled in Backend when a Task is moved

--- daily_kanban/frontend/views.py
+++ daily_kanban/frontend/views.py
@@ -6,7 +6,7 @@ from rest_framework.decorators import api_view
 from rest_framework import status
 from .models import Task
 from .serializers import TaskSerializer
-from datetime import timezone
+from django.utils import timezone
 
 
 # Create your views here.
@@ -37,38 +37,42 @@ class TaskListAPIView(APIView):
 def update_task_status(request, task_id):
     try:
         task = Task.objects.get(id=task_id)
-        task.status = request.data.get('status')
+        # task.status = request.data.get('status')
 
-        # new_status = request.data.get('status')
-
-        # if not isinstance(new_status, int) or new_status not in ['1', '2', '3']:
-        #     return Response({'error': 'Invalid status type or value'}, status=400)
+        new_status = request.data.get('status')
 
 
-        # # scenario 1: Task is created in Backlog
+        if not isinstance(new_status, str) or new_status not in ['0', '1', '2', '3']:
+            return Response({'error': 'Invalid status type or value'}, status=400)
+
+
+        # # scenario 1: Task is created in Backlog <- implement later after adding Task add functionality
         # if new_status == '0' and task.status != '0':
         #     task.date_created = timezone.now()
 
-        # # scenario 2-1: Task is moved from Backlog to another column (Today/In Progress)
-        # if task.status == '0' and new_status in ['1', '2']:
-        #     task.date_assigned = timezone.now()
+        # scenario 2-1: Task is moved from Backlog to another column (Today/In Progress)
+        if task.status == '0' and new_status in ['1', '2']:
+            task.date_assigned = timezone.now()
+            print(f'date_assigned: {task.date_assigned}')
 
-        # # scenario 2-2: Task is moved from Backlog to Done
-        # if task.status == '0' and new_status == '3':
-        #     task.date_assigned = timezone.now()
-        #     task.date_done = timezone.now()
+        # scenario 2-2: Task is moved from Backlog to Done
+        if task.status == '0' and new_status == '3':
+            task.date_assigned = timezone.now()
+            task.date_done = timezone.now()
+            print(f'date_assigned: {task.date_assigned}, date_done: {task.date_done}')
 
-        # # scenario 3: Task is moved to the next day
+        # scenario 3: Task is moved to the next day <- implement later
+        # date_moved
 
 
-        # # scenario 4: Task is moved to Done or out of Done
-        # if task.status in ['1', '2'] and new_status == '3': # moved to Done
-        #     task.date_done = timezone.now()
-        # elif task.status == '3' and new_status != '3': # moved out of Done
-        #     task.date_done = None
+        # scenario 4: Task is moved to Done or out of Done
+        if task.status in ['1', '2'] and new_status == '3': # moved to Done
+            task.date_done = timezone.now()
+        elif task.status == '3' and new_status in ['0', '1', '2']: # moved out of Done
+            task.date_done = None
 
-        # print(f"Old task status: {task.status}, New task status: {new_status}")
-        # task.status = new_status
+        print(f"Old task status: {task.status}, New task status: {new_status}")
+        task.status = new_status
 
         task.save()
 
