7bfe7b9dfe96c9ee4a72642d20b02221afe4f7ff
Soyeon <limcatcat@gmail.com>
2025-01-17 07:47:51 +0100

    set-up for live preview during development

--- daily_kanban/daily_kanban/settings.py
+++ daily_kanban/daily_kanban/settings.py
@@ -187,7 +187,8 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, 'frontend/static/frontend/')
 
 STATICFILES_DIRS = [
     BASE_DIR / "static", # project-level static files
