b1832642b39d3e9a499c9681ea63bccb5f54b9e6
Soyeon <limcatcat@gmail.com>
2025-01-25 03:39:57 +0100

    modified db config for go-live

--- daily_kanban/daily_kanban/settings.py
+++ daily_kanban/daily_kanban/settings.py
@@ -120,37 +120,37 @@ WSGI_APPLICATION = 'daily_kanban.wsgi.application'
 # }
 
 # in development
-# DOTENV_FILE = './.env'
-# env_config = Config(RepositoryEnv(DOTENV_FILE))
+DOTENV_FILE = './.env'
+env_config = Config(RepositoryEnv(DOTENV_FILE))
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': env_config.get('DB_NAME'),
-#         'USER': env_config.get('DB_USER'),
-#         'PASSWORD': env_config.get('DB_PASSWORD'),
-#         'HOST': env_config.get('DB_HOST'),
-#         'PORT': env_config.get('DB_PORT'),
-#     }
-# }
-
-# in production
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'NAME': env_config.get('DB_NAME'),
+        'USER': env_config.get('DB_USER'),
+        'PASSWORD': env_config.get('DB_PASSWORD'),
+        'HOST': env_config.get('DB_HOST'),
+        'PORT': env_config.get('DB_PORT'),
     }
 }
 
-if not any([os.getenv('DB_NAME'), os.getenv('DB_USER'), os.getenv('DB_PASSWORD')]):
-    DATABASES['default'] = {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+# in production
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST'),
+#         'PORT': os.getenv('DB_PORT'),
+#     }
+# }
+
+# if not any([os.getenv('DB_NAME'), os.getenv('DB_USER'), os.getenv('DB_PASSWORD')]):
+#     DATABASES['default'] = {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 
 
 # Password validation
